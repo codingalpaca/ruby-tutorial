edea25211d833a81f2587195174fa0909db65ef7
Arfullight <tmimvhunag@gmail.com>
2017-06-07 12:37:08 +0000

    course 8

--- guess.rb
+++ guess.rb
@@ -3,13 +3,18 @@
 # 3. 如果猜對了 -> 輸出 '猜對了'
 #    不然       -> 輸出 '猜錯了'
 
-number = Random.rand(9) + 1
+number = Random.rand(99) + 1
 
-print '請猜一個 1 ~ 10 之間的數字：'
-guess = gets.chomp.to_i
+while true
+  print '請猜一個 1 ~ 100 之間的數字：'
+  guess = gets.chomp.to_i
 
-if guess == number
-  puts '你猜對了，答案是 ' + number.to_s
-else
-  puts '你猜錯了！'
+  if guess == number
+    puts '你猜對了，答案是 ' + number.to_s
+    break
+  elsif guess > number
+    puts '猜太大了！'
+  else
+    puts '猜太小了！'
+  end
 end
