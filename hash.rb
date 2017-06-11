8815c29f96fed6314d4a4dc126a54bccd8799eb7
Arfullight <tmimvhunag@gmail.com>
2017-06-11 04:13:18 +0000

    course 11

---
+++ hash.rb
@@ -0,0 +1,9 @@
+record = {
+  :name  => 'John',
+  :score => 90
+}
+
+record.each do |key, value|
+  puts key
+  puts value
+end
--- score.rb
+++ score.rb
@@ -3,7 +3,10 @@
 # 3 -> 刪除成績
 # 4 -> 結束程式
 
-scores = [90, 80, 85]
+records = [
+  { :name => 'John', :score => 90 },
+  { :name => 'Alpaca', :score => 100 }
+]
 
 puts '輸入 1 檢視成績，2 新增成績，3 刪除成績，4 結束程式'
 
@@ -12,15 +15,22 @@ while true
   command = gets.chomp.to_i
 
   if command == 1
-    scores.each do |score|
-      puts score
+    records.each do |record|
+      puts record[:name] + '：' + record[:score].to_s + '分'
     end
   elsif command == 2
+    record = {}
+
+    print '請輸入學生的名字：'
+    record[:name] = gets.chomp
+
     print '請出入要新增的成績：'
-    scores.push gets.chomp.to_i
+    record[:score] = gets.chomp.to_i
+
+    records.push record
   elsif command == 3
-    print '請問要刪除第幾個成績：'
-    scores.delete_at gets.chomp.to_i - 1
+    print '請問要刪除第幾個紀錄：'
+    records.delete_at gets.chomp.to_i - 1
   elsif command == 4
     puts '程式結束！'
     break
